43fddc84c9824d3892dd1f699eecd91c13cc12da
Chip Methvin <cmethvin@indiana.edu>
2019-01-24 17:16:35 -0500

    add validation to number fields

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 export default ({ width, height, quantity, changeWidth, changeHeight, changeQuantity, addPiece, addEnabled, clearPieces }) => <form onSubmit={addPiece} autoComplete='off'>
   <label htmlFor='width'>Width</label>
-  <input type='text' id='width' value={width} onChange={changeWidth} />
+  <input type='text' id='width' value={width} onChange={changeWidth} pattern='[0-9]+([\.][0-9]+)?' />
   <label htmlFor='height'>Height</label>
-  <input type='text' id='height' value={height} onChange={changeHeight} />
+  <input type='text' id='height' value={height} onChange={changeHeight} pattern='[0-9]+([\.][0-9]+)?' />
   <label htmlFor='ply-quantity'>Quantity</label>
-  <input type='text' id='quantity' value={quantity} onChange={changeQuantity} />
+  <input type='text' id='quantity' value={quantity} onChange={changeQuantity} pattern='\d*' />
   <input type='submit' className='ply-button' value='Add' disabled={!addEnabled} />
   <button type='button' className='ply-button' onClick={clearPieces}>Clear</button>
 </form>
